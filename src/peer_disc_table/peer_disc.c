f7447cc5c897adcb62c612b52f8778286c8396d8
Andrea-P18 <andreapricoc@gmail.com>
2026-04-09 13:30:21 +0200

    Added peer_daemon to remove clients that did not send HELLO msg

--- src/peer_disc_table/peer_disc.c
+++ src/peer_disc_table/peer_disc.c
@@ -5,7 +5,10 @@
 #include <string.h>
 #include "peer_disc.h"
 #include "utils.h"
+#include <time.h>
+#include <pthread.h>
 
+#define TIMEOUT_SECONDS 10
 
 uint8_t peer_add(PeerDiscovery* peer_table, uint8_t* mac_address){
 
@@ -21,12 +24,17 @@ uint8_t peer_add(PeerDiscovery* peer_table, uint8_t* mac_address){
 
     for(int i = 0; i<MAX_PEERS; i++){
         /*If we find an empty node, we just add the mac_address and change the boolean flag to True*/
+        pthread_mutex_lock(&peer_table -> Node[i].lock_empty);
         if(peer_table -> Node[i].is_empty)
             {
                 peer_table -> Node[i].is_empty = false;
+                pthread_mutex_unlock(&peer_table -> Node[i].lock_empty);
                 memcpy(peer_table->Node[i].mac_address, mac_address, 6);
+                peer_table->Node[i].arrival_time = peer_table->global_timer;
+                peer_table->Node[i].snap_global_timer = peer_table->global_timer;
                 return 0;
             }
+        pthread_mutex_unlock(&peer_table -> Node[i].lock_empty);
         }
    
     return 3; /*No peers available*/
@@ -57,11 +65,40 @@ uint8_t peer_remove(PeerDiscovery* peer_table, uint8_t* mac_address){
     return 3; /*Mac address not found*/   
 }
 
-void peer_timers_increase(PeerDiscovery* peer_table, void* arg){
+void update_client_timer(DiscNode* Node){
+    clock_gettime(CLOCK_MONOTONIC, &Node->last_hello);
+}
 
-    while(true){
-        for(int i = 0; i<MAX_PEERS; i++)
-            peer_table->Node[i].timer -= 1;
-    }
+void *peer_daemon(void* arg){
+    /*This function controls that each client has been active in the last 10 seconds
+      else, it removes the client from the peer_table
+    */
+    PeerDiscovery* peer_table = (PeerDiscovery* ) arg;
+    while(1){
+        sleep(2);
+        struct timespec now;
+        clock_gettime(CLOCK_MONOTONIC, &now);
 
+        for(int i = 0; i < MAX_PEERS;i++){
+            pthread_mutex_lock(&peer_table -> Node[i].lock_empty);
+            bool empty = peer_table -> Node[i].is_empty;
+            pthread_mutex_unlock(&peer_table->Node[i].lock_empty); /*This variable MUST be protected by a mutex*/
+
+            if(empty) continue;
+            /*Elapsed controls if the now time minus arrival time is greater than TIMEOUT_SECONDS*/
+            double elapsed =
+                (now.tv_sec - peer_table -> Node[i].last_hello.tv_sec)+
+                (now.tv_nsec - peer_table -> Node[i].last_hello.tv_nsec) / 1e9;
+
+            if (elapsed > TIMEOUT_SECONDS){
+                printf("Client expired, node number %d has been removed\n",i);
+
+                pthread_mutex_lock(&peer_table -> Node[i].lock_empty)
+                peer_table -> Node[i].is_empty = true;
+                pthread_mutex_unlock(&peer_table -> Node[i].lock_empty);
+
+            }
+        }
+}
+return NULL;
 }
--- src/peer_disc_table/peer_disc.h
+++ src/peer_disc_table/peer_disc.h
@@ -6,23 +6,30 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "node.h"
-
+#include <time.h>
 
 #define MAX_PEERS 255
 
 
 typedef struct Disc_Node{
+
     bool is_empty; /*This boolean flag tells us if the node is empty or not: False if it is not empty, True otherwise*/
     uint8_t mac_address[6]; /*mac address is 6 bytes long.*/
+    struct timespec last_hello; /*last_hello sent from the client*/
+
+    pthread_mutex_t lock_empty; /*This mutex needs us to lock when we change is_empty's value*/
 }DiscNode;
 
-
 typedef struct Peer_Discovery{
+
     DiscNode Node[MAX_PEERS]; /*disc_node*/
+
 }PeerDiscovery;
+
+void update_client_timer(PeerDiscovery* peer_table); /*Updates timer for each client using cpu clocks*/
 
 uint8_t peer_add(PeerDiscovery* peer_table, uint8_t* mac_address); /*This function adds a peer in the discovery_table*/
 uint8_t peer_remove(PeerDiscovery* peer_table, uint8_t* mac_address); /*This function removes a peer in the discovery table*/
-void peer_timers_increase(PeerDiscovery* peer_table, void* arg);
+void peer_daemon(PeerDiscovery* peer_table, void* arg); /*This function removes peers that do not send the HELLO message*/
 
 #endif
